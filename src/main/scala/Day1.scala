242c0fae3e6f255045239a3ae69ba76ce6a6c7a4
Justin Reardon <justin.reardon@gmail.com>
2021-12-01 23:21:22 -0500

    Clean up duplicate code

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -2,16 +2,20 @@ import cats.effect.IO
 import fs2._
 
 object Day1:
-  def elements[F[_], I]: Pipe[F, I, Int] = stream =>
+  private def count[F[_], I]: Pipe[F, I, Int] = stream =>
     stream.fold(0)((sum, _) => sum + 1)
+
+  private def descends[F[_]](stream: Stream[F, Int]) =
+    stream
+      .sliding(2)
+      .filter(chunk => chunk(1) > chunk(0))
 
   // https://adventofcode.com/2021/day/1
   def a[F[_]]: Pipe[F, String, Int] = stream =>
     stream
       .map(_.toInt)
-      .sliding(2)
-      .filter(chunk => chunk(1) > chunk(0))
-      .through(elements)
+      .through(descends)
+      .through(count)
 
   // https://adventofcode.com/2021/day/1#part2
   def b[F[_]]: Pipe[F, String, Int] = stream =>
@@ -19,6 +23,5 @@ object Day1:
       .map(_.toInt)
       .sliding(3)
       .map(_.toList.sum)
-      .sliding(2)
-      .filter(chunk => chunk(1) > chunk(0))
-      .through(elements)
+      .through(descends)
+      .through(count)
