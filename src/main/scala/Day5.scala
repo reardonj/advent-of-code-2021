e3109e8b8bf277aa5bdd09070713ce8c7bbb3e5d
Justin Reardon <justin.reardon@gmail.com>
2021-12-05 11:45:45 -0500

    Day 5 solutions

---
+++ src/main/scala/Day5.scala
@@ -0,0 +1,55 @@
+import scala.annotation.tailrec
+object Day5 extends Day:
+  type Point = (Int, Int)
+  type Segment = (Point, Point)
+
+  def a(stream: Iterator[String]): Any =
+    stream
+      .map(lineToSegment)
+      .flatten
+      .filter(isStraight)
+      .flatMap(segmentToPoints)
+      .toSeq
+      .groupMapReduce(identity)(_ => 1)(_ + _)
+      .filter { case _ -> count => count > 1 }
+      .size
+
+  def b(stream: Iterator[String]): Any =
+    stream
+      .map(lineToSegment)
+      .flatten
+      .flatMap(segmentToPoints)
+      .toSeq
+      .groupMapReduce(identity)(_ => 1)(_ + _)
+      .filter { case _ -> count => count > 1 }
+      .size
+
+  private def isStraight(segment: Segment) = segment match {
+    case (x1, y1) -> (x2, y2) => x1 == x2 || y1 == y2
+  }
+
+  private def segmentToPoints(segment: Segment): Seq[Point] = {
+    @tailrec
+    def segmentToPoints(segment: Segment, acc: Seq[Point]): Seq[Point] =
+      segment match {
+        case (x1, y1) -> (x2, y2) =>
+          if x1 == x2 && y1 == y2 then acc
+          else
+            val nextPoint = (nextNum(x1, x2), nextNum(y1, y2))
+            segmentToPoints(nextPoint -> (x2, y2), acc :+ (x1, y1))
+      }
+
+    def nextNum(a: Int, b: Int) =
+      (if a < b then a + 1 else if a > b then a - 1 else a)
+
+    segmentToPoints(segment, Seq(segment._2))
+  }
+
+  // Pattern for: x1,y1 -> x2,y2
+  private val linePattern = """(\d*),(\d*)\s*->\s*(\d*),(\d*)""".r
+
+  private def lineToSegment(line: String): Option[Segment] = line match {
+    case linePattern(x1, y1, x2, y2) =>
+      Some((x1.toInt, y1.toInt) -> (x2.toInt, y2.toInt))
+    case _ => None
+  }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,17 +1,22 @@
 import java.nio.charset.Charset
 import scala.io.BufferedSource
 import scala.io.Source
+import scala.util.Try
 
 object AdventOfCode:
   @main def run(challengesToRun: String*) = for {
     challenge <- challengesToRun
+    (challengeFileName, pipeline) = challenges(challenge)
+    (name, file, solve) <- Seq(
+      (challenge + " example", challengeFileName + ".example", pipeline),
+      (challenge, challengeFileName, pipeline)
+    )
   } yield {
-    val (file, pipeline) = challenges(challenge)
-    println(challenge + ": " + pipeline(Source.fromResource(file).getLines))
+    Try(println(name + ": " + solve(Source.fromResource(file).getLines)))
   }
 
   val challenges: Map[String, (String, Iterator[String] => Any)] =
-    Seq(Day1, Day2, Day3, Day4).zipWithIndex.flatMap { case (day, idx) =>
+    Seq(Day1, Day2, Day3, Day4, Day5).zipWithIndex.flatMap { case (day, idx) =>
       day.challenges.map { case (name, challenge) =>
         val day = idx + 1
         s"${day}$name" -> (s"day${day}.txt", challenge)
