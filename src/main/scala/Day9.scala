84d9c08003d3bba7524b35bffb6d916fbf69f1c2
Justin Reardon <justin.reardon@gmail.com>
2021-12-09 15:43:40 -0500

    Day 9

---
+++ src/main/scala/Day9.scala
@@ -0,0 +1,96 @@
+import scala.collection.SeqLike
+import scala.annotation.tailrec
+object Day9 extends Day:
+  case class Point(x: Int, y: Int)
+
+  def a(stream: Iterator[String]): Any = {
+    val map = readMap(stream)
+    val lookup = toLookup(map)
+
+    val lowPoints = for {
+      y <- 0 until map.length
+      x <- 0 until map(0).length
+      height = map(y)(x)
+      neighbours = neighbourPoints(Point(x, y)).map(lookup).flatten
+      if neighbours.forall(height < _)
+    } yield {
+      //println(neighbours)
+      //println(s"point ($x, $y): $height")
+      height
+    }
+
+    lowPoints.map(_ + 1).sum
+  }
+
+  def b(stream: Iterator[String]): Any = {
+
+    val map = readMap(stream)
+    val lookup = toLookup(map)
+
+    val open = (for {
+      y <- 0 until map.length
+      x <- 0 until map(0).length
+    } yield {
+      Point(x, y)
+    }).toSet
+
+    @tailrec
+    def findBasins(
+        open: Set[Point],
+        basins: Seq[Set[Point]] = Seq()
+    ): Seq[Set[Point]] = {
+      if open.isEmpty then basins
+      else
+        val (rest, basin) =
+          buildBasin(Set(open.head), open - open.head, Set[Point]())
+        findBasins(rest, basins :+ basin)
+    }
+
+    @tailrec
+    def buildBasin(
+        frontier: Set[Point],
+        open: Set[Point],
+        basin: Set[Point]
+    ): (Set[Point], Set[Point]) =
+      if frontier.isEmpty then (open, basin)
+      else {
+        val next = frontier.head
+        lookup(next) match {
+          case None    => buildBasin(frontier - next, open, basin)
+          case Some(9) => buildBasin(frontier - next, open, basin)
+          case Some(_) =>
+            val neighbours = neighbourPoints(next).toSet.intersect(open)
+            buildBasin(
+              frontier - next ++ neighbours,
+              open -- neighbours,
+              basin + next
+            )
+        }
+      }
+
+    findBasins(open)
+      .filter(_.nonEmpty)
+      .map(_.size)
+      .sorted
+      .reverse
+      .take(3)
+      .fold(1)(_ * _)
+  }
+
+  private def neighbourPoints(point: Point) = point match {
+    case Point(x, y) =>
+      Seq(
+        Point(x - 1, y),
+        Point(x + 1, y),
+        Point(x, y + 1),
+        Point(x, y - 1)
+      )
+  }
+
+  private def readMap(stream: Iterator[String]) =
+    stream.map(_.map(_.intValue - '0').toArray).toArray
+
+  private def toLookup(array: Array[Array[Int]]) = {
+    val lookup = array.map(_.lift).lift
+    (point: Point) => lookup(point.y).map(_.apply(point.x)).flatten
+  }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -18,10 +18,19 @@ object AdventOfCode:
   }
 
   val challenges: Map[String, (String, Iterator[String] => Any)] =
-    Seq(Day1, Day2, Day3, Day4, Day5, Day6, Day7, Day8).zipWithIndex.flatMap {
-      case (day, idx) =>
-        day.challenges.map { case (name, challenge) =>
-          val day = idx + 1
-          s"${day}$name" -> (s"day${day}.txt", challenge)
-        }
+    Seq(
+      Day1,
+      Day2,
+      Day3,
+      Day4,
+      Day5,
+      Day6,
+      Day7,
+      Day8,
+      Day9
+    ).zipWithIndex.flatMap { case (day, idx) =>
+      day.challenges.map { case (name, challenge) =>
+        val day = idx + 1
+        s"${day}$name" -> (s"day${day}.txt", challenge)
+      }
     }.toMap
