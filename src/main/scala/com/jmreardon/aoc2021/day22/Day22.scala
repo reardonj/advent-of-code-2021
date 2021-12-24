81e2ed9c9b274d90764a13cadcffebe257b0e8b0
Justin Reardon <justin.reardon@gmail.com>
2021-12-23 22:05:33 -0500

    oct-tree implementation - doesn't handle 22b

--- src/main/scala/com/jmreardon/aoc2021/day22/Day22.scala
+++ src/main/scala/com/jmreardon/aoc2021/day22/Day22.scala
@@ -5,52 +5,102 @@ import cats.data.State
 
 object Day22 extends Day:
   def a(stream: Iterator[String]): Any =
-    parse(stream)
-      .takeWhile(_.region.containedBy(Region(-50 to 50, -50 to 50, -50 to 50)))
-      .foldLeft[Seq[Region]](Seq()) {
-        case (regions, RegionCommand(true, region)) =>
-          add(region, regions)
-        case (regions, RegionCommand(false, region)) =>
-          remove(region, regions)
-      }
-      .map(_.volume)
-      .sum
-
-  def b(stream: Iterator[String]): Any =
     val maxRegion = -131072 to 131072
     val baseTree: OctTree =
-      OctTreeNode(Region(maxRegion, maxRegion, maxRegion), false)
-    val commands = parse(stream).foldLeft(baseTree) {
-      case (tree, RegionCommand(on, region)) => tree.set(on, region)
-    }
+      EmptyOctTree(1, Region(maxRegion, maxRegion, maxRegion))
+    parse(stream)
+      .takeWhile(_.region.containedBy(Region(-50 to 50, -50 to 50, -50 to 50)))
+      .foldLeft(baseTree) {
+        case (tree, RegionCommand(true, region))  => tree.add(region)
+        case (tree, RegionCommand(false, region)) => tree.remove(region)
+      }
+      .volume
+
+  def b(stream: Iterator[String]): Any = 0
+
+  /*
+   Need a different algorithm
+   Start with no regions
+     - on adds the region to the list
+     - off does nothing
+   With one or more regions
+     - on add,
+       - find the intersection between the region and each existing region and add a negating region
+       - add the region
+     - on a remove, intersect with each region and add a negating operation
+   */
 
   private def rangeTotal(a: Range, b: Range) =
     Math.min(a.start, b.start) to Math.max(a.end, b.end)
 
   private trait OctTree:
+    val depth: Int
     val region: Region
-    def set(to: Boolean, cube: Region): OctTree
+    def volume: BigInt
+    def add(cube: Region): OctTree
+    def remove(cube: Region): OctTree
 
-  private case class OctTreeBranch(region: Region, children: Seq[OctTree])
+  private case class EmptyOctTree(depth: Int, region: Region) extends OctTree:
+    def volume: BigInt = BigInt(0)
+    def add(cube: Region): OctTree =
+      cube.within(region) match
+        case None         => this
+        case Some(within) => LeafOctTree(depth, region, within)
+
+    def remove(cube: Region): OctTree = this
+
+  private case class LeafOctTree(depth: Int, region: Region, on: Region)
       extends OctTree:
-    def set(to: Boolean, cube: Region): OctTree =
-      if region.containedBy(cube) then OctTreeNode(region, to)
-      else if region.intersects(cube) then
-        val updatedChildren = children.map(_.set(to, cube))
-        val setChildren = updatedChildren
-          .collect { case OctTreeNode(_, x) => x }
-          .count(_ == to)
-        if setChildren == 8 then OctTreeNode(region, to)
-        else OctTreeBranch(region, updatedChildren)
-      else this
 
-  private case class OctTreeNode(region: Region, on: Boolean) extends OctTree:
-    def set(to: Boolean, cube: Region): OctTree =
-      if region.containedBy(cube) then this.copy(on = to)
-      else if region.intersects(cube) then
-        val children = region.split.map(r => OctTreeNode(r, on))
-        OctTreeBranch(region, children).set(to, cube)
-      else this
+    def volume: BigInt = on.volume
+
+    def add(cube: Region): OctTree =
+      cube.within(region) match
+        case None => this
+        case Some(toAdd) =>
+          if toAdd.containedBy(on) then this
+          else BranchOctTree(depth, region, split.map(_.add(toAdd)))
+
+    def remove(cube: Region): OctTree =
+      cube.within(region) match
+        case None => this
+        case Some(toRemove) =>
+          if on.containedBy(toRemove) then EmptyOctTree(depth, region)
+          else BranchOctTree(depth, region, split.map(_.remove(toRemove)))
+
+    private def split = region.split.map { subRegion =>
+      //if depth < 10 then println(s"Splitting $this")
+      on.within(subRegion) match
+        case None        => EmptyOctTree(depth + 1, subRegion)
+        case Some(subOn) => LeafOctTree(depth + 1, subRegion, subOn)
+    }
+
+  private case class BranchOctTree(
+      depth: Int,
+      region: Region,
+      children: Seq[OctTree]
+  ) extends OctTree:
+    def volume: BigInt = children.map(_.volume).sum
+
+    def add(cube: Region): OctTree =
+      cube.within(region) match
+        case None => this
+        case Some(toAdd) =>
+          if region.containedBy(toAdd) then LeafOctTree(depth, region, region)
+          else this.copy(children = children.map(_.add(toAdd)))
+
+    def remove(cube: Region): OctTree =
+      cube.within(region) match
+        case None => this
+        case Some(toRemove) =>
+          if region.containedBy(toRemove) then EmptyOctTree(depth, region)
+          else
+            val updatedChildren = children.map(_.remove(toRemove))
+            if updatedChildren.collect { case EmptyOctTree(_, _) =>
+                1
+              }.sum == 8
+            then EmptyOctTree(depth, region)
+            else this.copy(children = updatedChildren)
 
   private def add(
       toAdd: Region,
@@ -82,8 +132,22 @@ object Day22 extends Day:
     def containedBy(that: Region): Boolean =
       containedBy(x, that.x) && containedBy(y, that.y) && containedBy(z, that.z)
 
+    private def containedBy(a: Range, b: Range) =
+      b.start <= a.start && b.end >= a.end
+
     def intersects(that: Region): Boolean =
       intersects(x, that.x) && intersects(y, that.y) && intersects(z, that.z)
+
+    def within(that: Region): Option[Region] =
+      val bound = Region(
+        boundRange(x, that.x),
+        boundRange(y, that.y),
+        boundRange(z, that.z)
+      )
+      Option.unless(bound.volume == 0)(bound)
+
+    private def boundRange(range: Range, bounds: Range) =
+      Math.max(range.start, bounds.start) to Math.min(range.end, bounds.end)
 
     def split: Seq[Region] =
       for
@@ -95,9 +159,6 @@ object Day22 extends Day:
     private def intersects(a: Range, b: Range) =
       b.contains(a.start) || b.contains(a.end) ||
         a.contains(b.start) || a.contains(b.end)
-
-    private def containedBy(a: Range, b: Range) =
-      b.contains(a.start) && b.contains(a.end)
 
     private def splitRange(range: Range): Seq[Range] =
       splitRangeAt(range, range.length / 2)
